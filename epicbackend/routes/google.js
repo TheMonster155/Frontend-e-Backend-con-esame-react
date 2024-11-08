b06c9687756474b69d553c13367e433eb141a626
TheMonster155 <136197595+TheMonster155@users.noreply.github.com>
2024-11-08 19:41:36 +0100

    google

--- epicbackend/routes/google.js
+++ epicbackend/routes/google.js
@@ -30,7 +30,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      callbackURL:
+        "https://backend-con-esame-react.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       return done(null, profile);
