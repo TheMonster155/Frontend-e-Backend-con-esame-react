ed625bc51020481d412c2d6225b78e707525447e
TheMonster155 <anass.grey@gmail.com>
2024-11-14 01:49:51 +0900

    senza github

--- Epicfrontend/src/App.jsx
+++ Epicfrontend/src/App.jsx
@@ -1,102 +1,4 @@
-/*import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage/HomePage'
-import About from './pages/About/About'
-import BookDay from './pages/BookDay/BookDay'
-import PageError from './pages/PageError/PageError'
-import BookDetails from './pages/BookDetails/BookDetails'
 
-import { useState } from 'react'
-
-import ProtectedRoutes from './Middwlwers/ProtectRoutes'
-import ContactPage from './pages/Contacts/Contacts'
-import Login from './pages/Login/Login'
-import SuccessLogin from './pages/SuccessLogin/SuccessLogin'
-import Cart from './pages/Card/Card'
-
-const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/about" element={<About />} />
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="*" element={<PageError />} />
-                <Route path="/homepage" element={<Navigate to="/" />} />
-                <Route path="/success" element={<SuccessLogin />} />
-
-             
-
-                <Route element={<ProtectedRoutes />}>
-                    <Route path="/card" element={<Cart />} />
-                    <Route path="/bookDay" element={<BookDay />} />
-                    <Route path="/contact" element={<ContactPage />} />
-
-                    <Route path="/book/:bookId" element={<BookDetails />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
-}
-
-export default App
-*/
-/*
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage/HomePage'
-import About from './pages/About/About'
-import BookDay from './pages/BookDay/BookDay'
-import PageError from './pages/PageError/PageError'
-import BookDetails from './pages/BookDetails/BookDetails'
-import ContactPage from './pages/Contacts/Contacts'
-import Login from './pages/Login/Login'
-
-import Cart from './pages/Card/Card'
-
-import ProtectedRoutes from './Middwlwers/ProtectRoutes'
-import { CartProvider } from './components/context/CardContext' // Importa CartProvider
-import GithubRedirect from './pages/GithubRedirect/GithubRedirect'
-
-const App = () => {
-    return (
-        <CartProvider>
-           
-            <BrowserRouter>
-                <Routes>
-                    <Route exact path="/" element={<HomePage />} />
-                    <Route path="/about" element={<About />} />
-
-                    <Route path="/login" element={<Login />} />
-
-                    <Route path="/homepage" element={<Navigate to="/" />} />
-               
-                    <Route
-                        path="/success/:token"
-                        element={<GithubRedirect />}
-                    />
-
-                    <Route element={<ProtectedRoutes />}>
-                        <Route path="/card" element={<Cart />} />
-                        <Route path="/bookDay" element={<BookDay />} />
-                        <Route path="/contact" element={<ContactPage />} />
-                        <Route path="/book/:bookId" element={<BookDetails />} />
-                    </Route>
-                    <Route path="*" element={<PageError />} />
-                </Routes>
-            </BrowserRouter>
-        </CartProvider>
-    )
-}
-
-export default App
-*/
-{
-    /* <Route path="/success/:token" element={<SuccessLogin />} />
-                   import SuccessLogin from './pages/SuccessLogin/SuccessLogin'*/
-}
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
@@ -111,7 +13,7 @@ import Login from './pages/Login/Login'
 import Cart from './pages/Card/Card'
 import ProtectedRoutes from './Middwlwers/ProtectRoutes'
 import { CartProvider } from './components/context/CardContext'
-import GithubRedirect from './pages/GithubRedirect/GithubRedirect'
+
 
 const App = () => {
     return (
@@ -123,8 +25,7 @@ const App = () => {
                     <Route path="/about" element={<About />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/homepage" element={<Navigate to="/" />} />
-                    <Route path="/success" element={<GithubRedirect />} />
-
+              
                     {/* Rotte protette */}
                     <Route element={<ProtectedRoutes />}>
                         <Route path="/card" element={<Cart />} />
